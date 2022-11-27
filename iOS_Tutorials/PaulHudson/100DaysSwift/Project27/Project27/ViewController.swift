393091866028090920c1dbcbb03f0b5f7eef6f7e
Michael Brockman <mbrockmandev@gmail.com>
2022-11-27 16:06:55 -0500

    another update. working on hws proj 27.

--- iOS_Tutorials/PaulHudson/100DaysSwift/Project27/Project27/ViewController.swift
+++ iOS_Tutorials/PaulHudson/100DaysSwift/Project27/Project27/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  var currentDrawType = 0
+  let num = 256
 
+  @IBOutlet weak var imageView: UIImageView!
+  
+  @IBAction func redrawTapped(_ sender: UIButton) {
+    currentDrawType += 1
+    
+    if currentDrawType > 5 {
+      currentDrawType = 0
+    }
+    
+    switch currentDrawType {
+    case 0:
+      drawRectangle()
+      
+    case 1:
+      drawCircle()
+      
+    default:
+      break
+    }
+    
+    
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    drawRectangle()
   }
-
-
+  
+  func drawRectangle() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: num, height: num))
+    
+    let img = renderer.image { context in
+      let rectangle = CGRect(x: 5, y: 5, width: num, height: num)
+      context.cgContext.setFillColor(UIColor.red.cgColor)
+      context.cgContext.setStrokeColor(UIColor.black.cgColor)
+      context.cgContext.setLineWidth(10)
+      
+      context.cgContext.addRect(rectangle)
+      context.cgContext.drawPath(using: .fillStroke)
+    }
+    imageView.image = img
+  }
+  
+  func drawCircle() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: num, height: num))
+    
+    let img = renderer.image { context in
+      let rectangle = CGRect(x: 5, y: 5, width: num, height: num)
+      context.cgContext.setFillColor(UIColor.red.cgColor)
+      context.cgContext.setStrokeColor(UIColor.black.cgColor)
+      context.cgContext.setLineWidth(10)
+      
+      context.cgContext.addEllipse(in: rectangle)
+      context.cgContext.drawPath(using: .fillStroke)
+    }
+    imageView.image = img
+  }
 }
-
