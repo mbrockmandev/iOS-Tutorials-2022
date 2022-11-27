7953f25f4008e2b11e8fe93c57644607f6b53227
Michael Brockman <mbrockmandev@gmail.com>
2022-11-27 12:51:16 -0500

    Pushing remaining changes

---
+++ iOS_Projects/MikeBrockman/HPIGen/HPIGen/HPIGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPIGenApp.swift
+//  HPIGen
+//
+//  Created by Michael Brockman on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct HPIGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
